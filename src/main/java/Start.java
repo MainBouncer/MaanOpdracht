f1a4008120d0282b89e4f8d4888072a3d60a119f
syh7 <sjoerd0707@live.nl>
2020-08-23 13:43:45 +0200

    Formatting

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,16 +1,12 @@
 import model.Administration;
 
 public class Start {
+
     public static void main(String[] args) {
         Administration administration = new Administration();
 
         int size = administration.getBorders().size();
         System.out.println(size);
-
-
     }
-    
-    
-    
-    
+
 }
