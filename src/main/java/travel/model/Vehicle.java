a6372844348d48156f9a371419f3cf02823fa612
syh7 <sjoerd0707@live.nl>
2020-08-28 15:40:43 +0200

    Add constructor with only VehicleType

--- src/main/java/travel/model/Vehicle.java
+++ src/main/java/travel/model/Vehicle.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import travel.enums.VehicleType;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +17,12 @@ public class Vehicle {
     private List<Cargo> cargoList;
     private Map<Integer, String> passengers;
     private VehicleType vehicleType;
+
+    public Vehicle(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+        cargoList = new ArrayList<>();
+        passengers = new HashMap<>();
+    }
 
     public Vehicle(List<Cargo> cargoList, Map<Integer, String> passengers, VehicleType vehicleType) {
         this.cargoList = cargoList;
