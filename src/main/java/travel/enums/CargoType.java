78df46a270bfc57032c05685c602af14ef8eece1
syh7 <sjoerd0707@live.nl>
2020-08-28 15:40:11 +0200

    Add getRandomCargoType function

--- src/main/java/travel/enums/CargoType.java
+++ src/main/java/travel/enums/CargoType.java
@@ -1,7 +1,14 @@
 package travel.enums;
 
+import java.util.Random;
+
 public enum CargoType {
     SOLID,
     LIQUID,
-    GAS
+    GAS;
+
+    public static CargoType getRandomCargoType() {
+        Random random = new Random();
+        return CargoType.values()[random.nextInt(CargoType.values().length)];
+    }
 }
