bba9763c7d08235acab6499d1a6a3047a33ea184
syh7 <sjoerd0707@live.nl>
2020-08-28 15:40:26 +0200

    Add name

--- src/main/java/travel/model/TravelCompany.java
+++ src/main/java/travel/model/TravelCompany.java
@@ -10,10 +10,12 @@ import java.util.List;
 @Setter
 @Builder
 public class TravelCompany {
+    private String name;
     private List<Voyage> voyageList;
     private List<Vehicle> vehicleList;
 
-    public TravelCompany(List<Voyage> voyageList, List<Vehicle> vehicleList) {
+    public TravelCompany(String name, List<Voyage> voyageList, List<Vehicle> vehicleList) {
+        this.name = name;
         this.voyageList = voyageList;
         this.vehicleList = vehicleList;
     }
