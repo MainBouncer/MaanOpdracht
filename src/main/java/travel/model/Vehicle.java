4020c76e3203dd7ed3182f598bab568087b369d2
syh7 <sjoerd0707@live.nl>
2020-09-03 13:35:41 +0200

    Change passenger to List, add method to load/unload cargo and people, add maximum size cargo and passenger lists

--- src/main/java/travel/model/Vehicle.java
+++ src/main/java/travel/model/Vehicle.java
@@ -6,27 +6,60 @@ import lombok.Setter;
 import travel.enums.VehicleType;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
 @Builder
 public class Vehicle {
+
+    private int maxCargoSize;
+    private int maxPassengerSize;
     private List<Cargo> cargoList;
-    private Map<Integer, String> passengers;
+    private List<String> passengers;
     private VehicleType vehicleType;
 
     public Vehicle(VehicleType vehicleType) {
-        this.vehicleType = vehicleType;
-        cargoList = new ArrayList<>();
-        passengers = new HashMap<>();
+        this(vehicleType, 10, 15);
     }
 
-    public Vehicle(List<Cargo> cargoList, Map<Integer, String> passengers, VehicleType vehicleType) {
+    public Vehicle(VehicleType vehicleType, int maxCargoSize, int maxPassengerSize) {
+        this(new ArrayList<>(), new ArrayList<>(), vehicleType, maxCargoSize, maxPassengerSize);
+    }
+
+    public Vehicle(List<Cargo> cargoList, List<String> passengers, VehicleType vehicleType, int maxCargoSize, int maxPassengerSize) {
+        if (cargoList == null || passengers == null) {
+            throw new IllegalArgumentException("No null lists!");
+        }
+        if (cargoList.size() > maxCargoSize || passengers.size() > maxPassengerSize) {
+            throw new IllegalArgumentException("Your passed list is bigger than allowed, dumby.");
+        }
         this.cargoList = cargoList;
         this.passengers = passengers;
         this.vehicleType = vehicleType;
+        this.maxCargoSize = maxCargoSize;
+        this.maxPassengerSize = maxPassengerSize;
+    }
+
+    public boolean enterPassenger(String passengerName) {
+        if (passengers.size() >= maxPassengerSize) {
+            return false;
+        }
+        return passengers.add(passengerName);
+    }
+
+    public boolean leavePassenger(String passengerName) {
+        return passengers.remove(passengerName);
+    }
+
+    public boolean loadCargo(Cargo cargo) {
+        if (cargoList.size() >= maxCargoSize) {
+            return false;
+        }
+        return cargoList.add(cargo);
+    }
+
+    public boolean unloadCargo(Cargo cargo) {
+        return cargoList.remove(cargo);
     }
 }
