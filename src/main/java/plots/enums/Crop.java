67f059cfe75d98ef8c2ee178f0de9b1b0d356fda
Jolein <joleinvandersluis@gmail.com>
2020-09-03 15:48:32 +0200

    crop callories added

--- src/main/java/plots/enums/Crop.java
+++ src/main/java/plots/enums/Crop.java
@@ -1,24 +1,33 @@
 package plots.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Crop {
-    TOMATO("Tomato", false),
-    CUCUMBER("Cucumber", false),
-    RADISH("Radish", false),
-    LETTUCE("Lettuce", false),
-    PAPRIKA("Paprika", false),
-    CANNABIS("Cannabis", true),
-    MOONSEED("Moonseed", true);
+    TOMATO("Tomato", false, 30),
+    CUCUMBER("Cucumber", false, 20),
+    RADISH("Radish", false, 5),
+    LETTUCE("Lettuce", false, 15),
+    PAPRIKA("Paprika", false, 25),
+    CANNABIS("Cannabis", true, 8),
+    MOONSEED("Moonseed", true, 35);
 
-    String name;
-    boolean isPermitRequired;
+    private String name;
+    private boolean isPermitRequired;
+    private int calories;
 
-    Crop(String name, boolean isPermitRequired) {
+    Crop(String name, boolean isPermitRequired, int calories) {
         this.name = name;
         this.isPermitRequired = isPermitRequired;
+        this.calories = calories;
     }
 
-    public String getName() {
-        return name;
+    public Crop getHighestCaloryCrop() {
+        Crop currentMax = Crop.values()[0];
+        for(Crop crop:Crop.values()) {
+            if (crop.getCalories() > currentMax.getCalories()) {
+                currentMax = crop
+        }
+        }
     }
-    public boolean isPermitRequired() {return isPermitRequired;}
 }
