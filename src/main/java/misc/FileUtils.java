4daa76212bf38c2c3d11bb92617d47d5620ec84a
MainBouncer <rogierrietvelt@hotmail.com>
2020-10-30 14:03:31 +0100

    Added FileUtils class with deleteStructure class

--- src/main/java/misc/FileUtils.java
+++ src/main/java/misc/FileUtils.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public class FileUtil {
+public class FileUtils {
     public static void deleteStructure(String parentDir) throws IOException {
         Path sourcePath = Paths.get(parentDir);
         Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>(){
